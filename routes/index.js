770e075fbb4333c8a29016310e3ce058b9dc5d7b
Railly <railly_2009@hotmail.com>
2022-11-04 15:35:17 -0500

    feat: add eslint + prettier config

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ import apiKeyRouter from './api-key'
 const router = express.Router()
 
 // router.use('/users', usersRouter)
-
 router.use('/key', apiKeyRouter)
 
 export default router
--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ class Server {
   constructor () {
     this.app = express()
     this.port = process.env.PORT
-
     this.middlewares()
     this.routes()
     this.dbConnection()
